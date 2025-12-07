2c633fdfb00cab2e2f54c3e9a5fad03a1e89c9e8
Neel Chatterjee <nchatterjee@wpi.edu>
2025-12-07 18:32:32 -0500

    pytest udpate from Kelvin

--- tests/test_config.py
+++
@@ -1,4 +0,0 @@
-from cmapss_rul import config
-
-def test_default_config_has_expected_arch():
-    assert config.DEFAULT.arch in ["cnn", "lstm", "tcn"]
--- tests/test_eval.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from cmapss_rul import eval
-
-def test_cmapss_score_runs():
-    true = [50, 40, 30]
-    pred = [55, 35, 32]
-    score = eval.cmapss_score(true, pred)
-    assert score >= 0
